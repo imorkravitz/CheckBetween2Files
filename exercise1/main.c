a3a3c1acc78e3d76091fb160eccdb76c9229f7e6
Or Kravitz <imorkravitz@gmail.com>
2021-12-07 22:56:01 +0200

    change some code

--- exercise1/main.c
+++ exercise1/main.c
@@ -10,20 +10,32 @@
 int main(int argc, char *argv[]){
     
     int res1, res2, res3;
-  
-    char input[50];
-    char expectedOutput[50];  
-    char students[50]; // "./students"
-    int fd1 =  open("./config.txt", O_RDONLY);
+    char const* const fileName = "./config.txt";
+    FILE* file = fopen(fileName, "r");
+    if (!file){
+        printf("Error opening: %s", fileName);
+        return -1;
+    }
+    char line[500];
+    while(fgets(line,sizeof(line),file)){
+        printf("%s",line);
+    }  
+    fclose(file);
+
+
+    // char input[50];
+    // char expectedOutput[50];  
+    // char students[50]; // "./students"
+    // int fd1 =  open("./config.txt", O_RDONLY);
     
-    res1 = read(fd1, &students, 50); // ./students
-    res2 = read(fd1, &input, 50); // ./input
-    res3 = read(fd1, &expectedOutput, 50); // ./expectedOutput
+    // res1 = read(fd1, &students, 50); // ./students
+    // res2 = read(fd1, &input, 50); // ./input
+    // res3 = read(fd1, &expectedOutput, 50); // ./expectedOutput
 
    
-    printf("%s", students);
+    // printf("%s", students);
 
-    close(fd1);
+    // close(fd1);
 
-   return 1;
+   return 0;
 }
