778b87cbf3c91f95c9db3edad3bd2642ab19ccbc
Or Kravitz <imorkravitz@gmail.com>
2021-12-09 21:27:01 +0200

    finished the main Main

--- exercise1/main.c
+++ exercise1/main.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
 
     char const *const fileName = "./config.txt";
     FILE *file = fopen(fileName, "r");
-    if (!file){
+    if (!file)
+    {
         printf("Error opening: %s", fileName);
         return -1;
     }
@@ -35,34 +36,88 @@ int main(int argc, char *argv[])
 
     //-------------------------------//
 
-int fd, pid, ret_code;
-fd = open ( "studentList.txt" , (O_WRONLY | O_CREAT | O_TRUNC) , 0666);
-  
-pid = fork();  
-if (pid == 0) /* son */ 
-{	
-	printf("Son : %d ;file descriptor: %d \n",getpid(),fd);
-	close(1);	/* close stdout*/
-	dup(fd);	/* dup will copy fd into stdout */
-	close(fd);	/* no need for fd anymore*/
-    
-    char* args[3];
-    args[0] = "ls";
-    args[1] = "students";
-    args[2] = NULL;
-    ret_code = execvp(args[0], args);
-	// ret_code = execvp(args[0], args);	/*always same exe */  
-	if (ret_code == -1) {
-		perror("exec failed "); 
-		exit(-1);
-	} 
-} 
-else	/* father */ 
-{  
-	printf("Father: after fork, sons proc id is %d \n", pid);
+    int fd, pid, ret_code;
+    fd = open("studentList.txt", (O_WRONLY | O_CREAT | O_TRUNC), 0666);
+    pid = fork();
+    if (pid == 0) /* son */
+    {
+        printf("Son : %d ;file descriptor: %d \n", getpid(), fd);
+        close(1);  /* close stdout*/
+        dup(fd);   /* dup will copy fd into stdout */
+        close(fd); /* no need for fd anymore*/
 
-	wait(NULL);	
-}
+        char *args[3];
+        args[0] = "ls";
+        args[1] = "students";
+        args[2] = NULL;
+        ret_code = execvp(args[0], args);
+        // ret_code = execvp(args[0], args);	/*always same exe */
+        if (ret_code == -1)
+        {
+            perror("exec failed ");
+            close(fd);
+            exit(-1);
+        }
+    }
+    else /* father */
+    {
+        printf("Father: after fork, sons proc id is %d \n", pid);
+        wait(NULL);
+    }
+    int grades = open("grades.txt", (O_WRONLY | O_CREAT | O_TRUNC), 0666);
+    if (grades < 0)
+    {
+        perror("after open");
+        _exit(1);
+    }
+    int output = open("output.txt", (O_WRONLY | O_CREAT | O_TRUNC), 0666);
+    if (output < 0)
+    {
+        perror("after open");
+        _exit(1);
+    }
+    char const *const studentsList = "./studentList.txt";
+    FILE *file2 = fopen(studentsList, "r");
+    if (!file2)
+    {
+        printf("Error opening: %s", fileName);
+        return -1;
+    }
+    char line2[50];
+    while (fgets(line2, sizeof(line), file2))
+    {
+        int fd2, pid2, ret_code2;
+        pid2 = fork();
+        if (pid2 == 0) /* son */
+        {
+            printf("Son : %d ;file descriptor: %d \n", getpid(), fd);
+            close(1);   /* close stdout*/
+            dup(fd2);   /* dup will copy fd into stdout */
+            close(fd2); /* no need for fd anymore*/
 
+            char *args2[5];
+            args2[0] = "cd";
+            args2[1] = "students/roy";
+            args2[2] = "./main.exe";
+            args2[3] = "input.txt";
+            args2[4] = NULL;
+            ret_code2 = execvp(args2[0], args2);
+            // ret_code = execvp(args[0], args);	/*always same exe */
+            if (ret_code2 == -1)
+            {
+                perror("exec failed ");
+                close(fd2);
+                exit(-1);
+            }
+            else /* father */
+            {
+                printf("Father: after fork, sons proc id is %d \n", pid2);
+                wait(NULL);
+            }
+        }
+        //char Buff[50];
+        //strcpy(Buff, line2);
+        //printf("%s\n", line2);
+    }
     return 0;
 }
